97036b534ce28d08502c16b958d435594899fa9b
Timur Kakhanovich <timkakhanovich@gmail.com>
2023-06-20 22:53:02 +0200

    Refactor data_preprocessing module

--- ASR/modules/data/data_processing.py
+++ ASR/modules/data/data_processing.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, Any
+from typing import Any, Dict, Union
 
 import torch
 import torch.nn as nn
@@ -8,9 +8,9 @@ import torchaudio
 class Unsqueeze(nn.Module):
     def __init__(self, dim: int = 0):
         super().__init__()
-        
+
         self.dim = dim
-    
+
     def forward(self, x: torch.Tensor) -> torch.FloatTensor:
         return x.unsqueeze(self.dim)
 
@@ -26,43 +26,44 @@ class ASRProcessor:
         split: str = 'val',
     ):
         self.mel_spec_processor = torchaudio.transforms.MelSpectrogram(
-            sample_rate=sampling_rate, 
-            n_fft=n_fft, 
-            hop_length=hop_length, 
-            n_mels=n_mels
+            sample_rate=sampling_rate, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels
         )
-        
+
         if split == 'train':
             self.augmentation = nn.Sequential(
-                Unsqueeze(), 
-                torchaudio.transforms.TimeMasking(time_mask_param=5), 
-                torchaudio.transforms.FrequencyMasking(freq_mask_param=5), 
+                Unsqueeze(),
+                torchaudio.transforms.TimeMasking(time_mask_param=5),
+                torchaudio.transforms.FrequencyMasking(freq_mask_param=5),
             )
         else:
             self.augmentation = None
-        
+
         self.tokenizer = tokenizer
-        
+
     def text_processor(self, text: str) -> torch.LongTensor:
-        encoded_text = [self.tokenizer.BOS] + \
-                       self.tokenizer.encode_ids(text) + \
-                       [self.tokenizer.EOS]
-        
+        encoded_text = (
+            [self.tokenizer.BOS]
+            + self.tokenizer.encode_ids(text)
+            + [self.tokenizer.EOS]
+        )
+
         return torch.tensor(encoded_text, dtype=torch.int64)
-    
+
     def __call__(
         self,
         input_values: torch.FloatTensor,
         labels: str,
     ) -> Dict[str, Union[torch.FloatTensor, torch.LongTensor]]:
-        # Got such boundaries for 99.8% of non-augmented train data: [-10.7776, 6.4294].  
-        log_mel_spec_image = torch.log(self.mel_spec_processor(input_values) + 1e-6).clamp_(-10, 6)
-        input_preprocessed = self.augmentation(log_mel_spec_image).squeeze(0) \
-                                if self.augmentation else log_mel_spec_image
-        
+        # Got such boundaries for 99.8% of non-augmented train data: [-10.7776, 6.4294].
+        log_mel_spec_image = torch.log(
+            self.mel_spec_processor(input_values) + 1e-6
+        ).clamp_(-10, 6)
+        input_preprocessed = (
+            self.augmentation(log_mel_spec_image).squeeze(0)
+            if self.augmentation
+            else log_mel_spec_image
+        )
+
         text_preprocessed = self.text_processor(labels)
-        
-        return {
-            'input_features': input_preprocessed, 
-            'labels': text_preprocessed
-        }
+
+        return {'input_features': input_preprocessed, 'labels': text_preprocessed}
